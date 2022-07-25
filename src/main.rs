01c7d7965f5306412403ed801d11d96c6102b140
Thomas A <curioustommy@protonmail.com>
2022-07-24 19:16:46 -0700

    Check for "System/iOSSupport" Path

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ mod symbols;
 
 use std::path::Path;
 
+fn analyse_system_path<P: AsRef<Path>>(path: &P, results_location: &location::ResultsLocation, whoami_username: &program::WhoAmIUserName, unique_folder :Option<&str>) {
+    let parse_filesystem_symbols_list = symbols::ParseBaseFilesystem::new(path);
+    for parse_filesystem_symbols in parse_filesystem_symbols_list {
+        
+        parse_filesystem_symbols.traverse(&results_location.shared_cache_path, unique_folder, path, &whoami_username);
+    }
+}
+
 fn main() {
     let arguments = argument::Arguments::new(std::env::args());
     let base_locations = location::BaseLocation::new(&arguments);
@@ -19,20 +27,18 @@ fn main() {
     let dyld_shared_cache_extractor = program::DyldSharedCacheExtractor::new(&base_locations, &results_location);
     
     for path in dyld_shared_cache_extractor.extracted_paths.iter() {
-        let parse_filesystem_symbols = symbols::ParseBaseFilesystem::new(path);
-        let shared_cache_folder = path.file_name().expect("Unable to obtain shared cache folder name").to_str();
-        parse_filesystem_symbols.traverse(&results_location.shared_cache_path, shared_cache_folder, path, &whoami_username);
+        let shared_cache_folder = path.as_path().file_name().expect("Unable to obtain shared cache folder name").to_str();
+        analyse_system_path(path,&results_location,&whoami_username,shared_cache_folder);
     }
-
     clean::Cleanup::remove_temp(&results_location);
 
     {
         let path = Path::new(arguments.base_path.as_str());
-        let parse_filesystem_symbols = symbols::ParseBaseFilesystem::new(path);
-        parse_filesystem_symbols.traverse(&results_location.unique_version_path, Some("standard"), path, &whoami_username);
+        let unique_folder = Some("standard");
+        analyse_system_path(&path,&results_location,&whoami_username,unique_folder);
+        clean::Cleanup::remove_temp(&results_location);
     }
-    
-    clean::Cleanup::remove_temp(&results_location);
+
 
     println!{"{:#?}",arguments}
     println!{"{:#?}",system_version}
